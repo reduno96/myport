b19dd169e02f60e64869c9bc9498b6600fc02d36
reduno96 <reduno96@gmail.com>
2023-05-23 19:29:06 +0100

    Modification

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-// import { Skills } from "./components/Skills";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
 // import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
 // import { Footer } from "./components/Footer";
@@ -12,8 +13,9 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      {/* <Skills />
-      <Projects />
+      <About />
+      <Skills />
+      {/* <Projects />
       <Contact />
       <Footer /> */}
     </div>
