47b4083481f3ab361ed036bad2cbb819d849a2fe
reduno96 <reduno96@gmail.com>
2023-11-21 14:24:34 +0100

    modification

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
-import navIcon4 from "../assets/img/nav-icon4.svg";
-import navIcon5 from "../assets/img/nav-icon5.svg";
 
 import { HashLink } from "react-router-hash-link";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -94,52 +89,6 @@ export const NavBar = () => {
               </Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon">
-                <a
-                  className="linkedin"
-                  href="https://www.linkedin.com/in/redouane-elmorabet/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={navIcon1} alt="" />
-                </a>
-                <a
-                  className="github"
-                  href="https://github.com/reduno96"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={navIcon4} alt="" />
-                </a>
-                <a
-                  className="facebook"
-                  href="https://www.facebook.com/redouane.morabet"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={navIcon2}
-                    alt="https://www.instagram.com/redouane_el_morabet/"
-                    target="_blank"
-                  />
-                </a>
-                <a
-                  className="instagram"
-                  href="https://www.instagram.com/redouane_el_morabet/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={navIcon3} alt="" />
-                </a>
-                <a
-                  className="twitter"
-                  href="https://twitter.com/MotivRed"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={navIcon5} alt="" />
-                </a>
-              </div>
               <HashLink to="#connect">
                 <button className="button-connect">
                   <span>Let’s Connect</span>
